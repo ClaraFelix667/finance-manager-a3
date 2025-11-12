4a71bcee749896cb95a1da1254a6ded9afc30ccb
Clara Felix <m.claraalmeidafelix@gmail.com>
2025-11-11 22:24:58 -0300

    add diagrama de atividades

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,12 @@
 
 -- Tabela de Usuarios
--- Armazena os dados principais do usuário
+-- Armazena os dados do usuário
 CREATE TABLE Usuarios (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL UNIQUE,
-    Senha VARCHAR(255) NOT NULL COMMENT 'Armazenar como HASH'
-) COMMENT='Armazena os usuários do sistema.';
+    Senha VARCHAR(255) NOT NULL COMMENT,
+);
 
 --Tabela de contas
 CREATE TABLE Contas (
@@ -16,7 +16,7 @@ CREATE TABLE Contas (
     Tipo VARCHAR(50) COMMENT 'Ex: Conta Corrente, Poupança, Cartão de Crédito',
     SaldoInicial DECIMAL(10, 2) DEFAULT 0.00,
     FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID) ON DELETE CASCADE
-) COMMENT='Contas bancárias e cartões de cada usuário.';
+);
 
 -- Tabela de transações
 -- armazena todas as receitas e despesas
@@ -36,22 +36,22 @@ CREATE TABLE Transacoes (
 CREATE TABLE Orcamentos (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     ID_Usuario INT NOT NULL,
-    Mes DATE NOT NULL COMMENT 'Armazena o primeiro dia do mês (YYYY-MM-01)',
+    Mes DATE NOT NULL COMMENT,
     Categoria VARCHAR(100) NOT NULL,
     Limite DECIMAL(10, 2) NOT NULL,
     Gasto_Atual DECIMAL(10, 2) DEFAULT 0.00,
     FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID) ON DELETE CASCADE,
     UNIQUE(ID_Usuario, Mes, Categoria)
-) COMMENT='Orçamentos mensais por categoria.';
+);
 
 CREATE TABLE Relatorios (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     ID_Usuario INT NOT NULL,
     Tipo ENUM('MENSAL', 'ANUAL') NOT NULL,
     DataGeracao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Dados JSON NOT NULL COMMENT 'Dados (JSON gráficos) conforme requisito',
+    Dados JSON NOT NULL,
     FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID) ON DELETE CASCADE
-) COMMENT='Relatórios gerados pelo sistema.';
+);
 
 CREATE TABLE Integracoes (
     ID INT PRIMARY KEY AUTO_INCREMENT,
@@ -62,4 +62,5 @@ CREATE TABLE Integracoes (
     UltimaSincronizacao DATETIME,
     FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID) ON DELETE CASCADE,
     FOREIGN KEY (ID_Conta) REFERENCES Contas(ID) ON DELETE CASCADE
-) COMMENT='Armazena dados de integração com APIs de bancos.';
+);
+
